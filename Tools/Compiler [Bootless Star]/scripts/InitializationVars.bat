54248492ac82eb5f820d7c2bd2d0263b3df59dd6
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2016-06-30 19:19:35 -0500

    Revised the compiler info
    Compiler version is 1 for this project.

--- Tools/Compiler [Bootless Star]/scripts/InitializationVars.bat
+++ Tools/Compiler [Bootless Star]/scripts/InitializationVars.bat
@@ -46,8 +46,8 @@ REM # ==========================================================================
 REM Static Variables
 SET ProjectName=Shadowmaker
 SET ProjectNameShort=Shadowmaker
-SET ProjectVersion=5
-SET ReleaseDate=12.March.2016
+SET ProjectVersion=1
+SET ReleaseDate=30.June.2016
 SET ProjectNameCompact=shdwmkr
 REM Used as the filename and directory when compiling the project.  This is built in the compiling protocol, just let it as 'UNKNOWN' here and change it later.
 SET FileName=UNKNOWN
